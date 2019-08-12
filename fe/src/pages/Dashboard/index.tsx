dc5034ced092c7d3e8876910d61c59802b6757ca
Junhong <raimonfuns@gmail.com>
2019-08-12 15:20:27 +0800

    tweak: dashboard and editor

--- fe/src/pages/Dashboard/index.tsx
+++ fe/src/pages/Dashboard/index.tsx
@@ -4,18 +4,28 @@ import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { useStore } from '../../store';
 
-export default observer(() => {
+export default observer((props: any) => {
   const store = useStore();
 
   const logout = () => {
     window.location.href = `http://localhost:8090/logout?from=http://localhost:4200/login`;
   };
 
+  if (store.user.isFetched && !store.user.isLogin) {
+    setTimeout(() => {
+      props.history.push('/login');
+    }, 0);
+  }
+
   return (
     <div>
       <h2>dashboard</h2>
-      <img className="push-top" src={store.user.avatar} width="100" alt="头像" />
-      <div className="push-top">{store.user.name}</div>
+      {store.user.isLogin && (
+        <div>
+          <img className="push-top" src={store.user.avatar} width="100" alt="头像" />
+          <div className="push-top">{store.user.name}</div>
+        </div>
+      )}
       <Link to="/editor">
         <Button className="push-top" variant="contained" color="primary">
           编辑器
--- fe/src/pages/Editor/index.tsx
+++ fe/src/pages/Editor/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { useStore } from '../../store';
 
-export default observer(() => {
+export default observer((props: any) => {
+  const store = useStore();
+
+  if (store.user.isFetched && !store.user.isLogin) {
+    setTimeout(() => {
+      props.history.push('/login');
+    }, 0);
+  }
+
   return (
     <div>
       <div>编辑器</div>
