17f54fba46bd69e63d98bcd1a1a7c56ecef977b8
Junhong <raimonfuns@gmail.com>
2019-08-23 09:58:55 +0800

    feat: add intro js & tweak style

--- fe/src/pages/Dashboard/index.tsx
+++ fe/src/pages/Dashboard/index.tsx
@@ -2,18 +2,32 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
 
-import { Button, MenuItem, Paper, Popover, Table, TableHead, TableBody, TableRow, TableCell, MenuList } from '@material-ui/core';
+import {
+  Button,
+  MenuItem,
+  Paper,
+  Popover,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  MenuList,
+} from '@material-ui/core';
 
 import CreateIcon from '@material-ui/icons/Create';
 import ExpandLess from '@material-ui/icons/ExpandLess';
+
+import introJs from 'intro.js';
+import 'intro.js/introjs.css';
 
 import Api from '../../api';
 
 import { useStore } from '../../store';
 
-import { ago, Endpoint } from '../../utils'
+import { ago, Endpoint } from '../../utils';
 
-import './index.scss'
+import './index.scss';
 
 export default observer((props: any) => {
   const store = useStore();
@@ -31,24 +45,45 @@ export default observer((props: any) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   React.useEffect(() => {
-    Api.getFiles().then(files => store.files.setFiles(files)).catch(console.error);
-  }, [store.files.files.length]);
+    (async () => {
+      const files = await Api.getFiles();
+      store.files.setFiles(files);
+      if (files.length === 0) {
+        tryShowIntro();
+      }
+    })();
+  }, [store]);
+
+  const tryShowIntro = () => {
+    const consumed = localStorage.getItem('INTRO_CONSUMED');
+    if (consumed) {
+      return;
+    }
+    introJs()
+      .setOption('hintButtonLabel', '我知道了')
+      .addHints()
+      .onhintclose(() => {
+        localStorage.setItem('INTRO_CONSUMED', 'true');
+      });
+  };
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
-  }
+  };
 
   const handleClose = () => {
     setAnchorEl(null);
-  }
+  };
 
   const handleClickTable = (fileId: number) => {
     props.history.push(`/editor?id=${fileId}`);
-  }
+  };
 
   const handleDelete = (id: number) => {
-    Api.deleteFile(id).then(() => store.files.setFiles(store.files.files.filter((item: any) => +item.id !== id))).catch(console.error);
-  }
+    Api.deleteFile(id)
+      .then(() => store.files.setFiles(store.files.files.filter((item: any) => +item.id !== id)))
+      .catch(console.error);
+  };
 
   return (
     <div className="p-dashboard flex">
@@ -58,14 +93,14 @@ export default observer((props: any) => {
             <li className="p-dashboard-nav-ul-title p-dashboard-nav-li">管理</li>
             <li className="p-dashboard-nav-li">
               <div className="p-dashboard-nav-link flex v-center po-bold po-radius-5">
-                <CreateIcon className="p-dashboard-nav-li-icon"/>
+                <CreateIcon className="p-dashboard-nav-li-icon" />
                 文章
               </div>
             </li>
           </ul>
         </section>
 
-        { store.user.isLogin && (
+        {store.user.isLogin && (
           <Button
             className="p-dashboard-nav-button flex"
             aria-controls="dashboard-menu"
@@ -82,7 +117,7 @@ export default observer((props: any) => {
           </Button>
         )}
 
-        { store.user.isLogin && (
+        {store.user.isLogin && (
           <Popover
             id="dashboard-menu"
             className="p-dashboard-popover"
@@ -100,7 +135,9 @@ export default observer((props: any) => {
             }}
           >
             <MenuList>
-              <MenuItem dense onClick={logout}>Logout</MenuItem>
+              <MenuItem dense onClick={logout}>
+                Logout
+              </MenuItem>
             </MenuList>
           </Popover>
         )}
@@ -111,7 +148,15 @@ export default observer((props: any) => {
           <div className="p-dashboard-main-head-title">文章</div>
 
           <Link to="/editor">
-            <Button className="p-dashboard-main-button">创建文章</Button>
+            <Button
+              className="primary"
+              variant="contained"
+              color="primary"
+              data-hint="点击创建你的第一篇文章，发布到区块链上吧～"
+              data-position="bottom-left-aligned"
+            >
+              创建文章
+            </Button>
           </Link>
         </section>
 
@@ -128,18 +173,34 @@ export default observer((props: any) => {
               </TableHead>
 
               <TableBody>
-                {
-                  store.files.files.map((file: any) => (
-                    <TableRow hover key={file.id} onClick={() => { handleClickTable(+file.id) }}>
-                      <TableCell component="th" scope="row">{file.title}</TableCell>
-                      <TableCell>{ago(file.updatedAt)}</TableCell>
-                      <TableCell>{file.status}</TableCell>
-                      <TableCell>
-                        <Button size="small" variant="contained" color="secondary" onClick={e => { e.stopPropagation();handleDelete(+file.id) }} >删除</Button>
-                      </TableCell>
-                    </TableRow>
-                  ))
-                }
+                {store.files.files.map((file: any) => (
+                  <TableRow
+                    hover
+                    key={file.id}
+                    onClick={() => {
+                      handleClickTable(+file.id);
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {file.title}
+                    </TableCell>
+                    <TableCell>{ago(file.updatedAt)}</TableCell>
+                    <TableCell>{file.status}</TableCell>
+                    <TableCell>
+                      <Button
+                        size="small"
+                        variant="contained"
+                        color="secondary"
+                        onClick={e => {
+                          e.stopPropagation();
+                          handleDelete(+file.id);
+                        }}
+                      >
+                        删除
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
               </TableBody>
             </Table>
           </Paper>
--- fe/src/store/files.ts
+++ fe/src/store/files.ts
@@ -1,4 +1,5 @@
 interface FilesStore {
+  isFetched: Boolean;
   files: Array<any>;
   setFiles: Function;
   updateFiles: Function;
@@ -6,16 +7,18 @@ interface FilesStore {
 
 export function createFilesStore() {
   const filesStore: FilesStore = {
+    isFetched: false,
     files: [],
     setFiles(files: any) {
+      this.isFetched = true;
       this.files = files;
     },
     updateFiles(file: any) {
       this.files = this.files.map(item => {
         if (+item.id === +file.id) item.status = file.status;
         return item;
-      })
-    }
-  }
+      });
+    },
+  };
   return filesStore;
 }
