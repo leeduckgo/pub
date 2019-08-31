ad028b4e93e8b509e7eb71769907320d79722f70
Junhong <raimonfuns@gmail.com>
2019-08-31 10:13:25 +0800

    update

---
+++ be/test/cases/api_topic.js
@@ -0,0 +1,29 @@
+require('should');
+const config = require('../../config');
+const api = require('../api');
+
+const generateName = () => 'test_topic' + new Date().getTime();
+
+it('can not create file when missing name', () => {
+  const topic = {
+    name: generateName(),
+  };
+  return api.post(`/api/topics`)
+    .send({
+      payload: topic
+    })
+    .set('Cookie', [`${config.authTokenKey}=${global.token}`])
+    .expect(400);
+});
+
+it('create topic', () => {
+  const topic = {
+    name: generateContent(),
+  };
+  return api.post(`/api/topics`)
+    .send({
+      payload: topic
+    })
+    .set('Cookie', [`${config.authTokenKey}=${global.token}`])
+    .expect(200);
+});
