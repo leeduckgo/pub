540dbafd0fcaaa2f13141b8ed6473b8d1b7a026a
Junhong <raimonfuns@gmail.com>
2019-09-12 12:06:25 +0800

    tweak: build.sh

--- build_cn.sh
+++ build_cn.sh
@@ -1,5 +1,12 @@
-image_name="dh-cn.press.one/pressone/pub"
+cd /pressone/pub
+git reset --hard HEAD
+git pull
+git checkout release
+git clean -f -d
+git pull
+
+IMAGE_NAME="dh-cn.press.one/pressone/pub"
 
 sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
-sudo docker build -f Dockerfile -t "$image_name" .
-sudo docker push "$image_name"
+sudo docker build -f Dockerfile -t $IMAGE_NAME .
+sudo docker push $IMAGE_NAME
