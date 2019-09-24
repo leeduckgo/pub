0547cac6bc93a02612ca544453b63fb44b389051
Junhong <raimonfuns@gmail.com>
2019-09-24 14:11:30 +0800

    tweak: send log to bot

--- be/models/log.js
+++ be/models/log.js
@@ -1,17 +1,38 @@
+const request = require('request-promise');
 const User = require('./user');
 const Log = require('./sequelize/log');
+const config = require('../config');
 
 exports.create = async (userId, message) => {
   const user = await User.get(userId);
-  await Log.create({
+  const data = {
     userId,
     message: `${user.name}：${message}`,
-  })
+  };
+  await Log.create(data);
+  if (config.botEnabled) {
+    sendToBot(data);
+  }
 }
 
 exports.createAnonymity = async (identity, message) => {
-  await Log.create({
+  const data = {
     userId: 0,
     message: `${identity}：${message}`,
-  })
+  };
+  await Log.create(data);
+  if (config.botEnabled) {
+    sendToBot(data);
+  }
 }
+
+const sendToBot = async data => {
+  request({
+    uri: config.botUrl,
+    method: 'post',
+    json: true,
+    body: {
+      payload: data
+    }
+  });
+};
