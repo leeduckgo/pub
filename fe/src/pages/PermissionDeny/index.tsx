8067ba0eeaf140496b9a3c747df8fbb6b392418c
Junhong <raimonfuns@gmail.com>
2019-11-12 12:29:20 +0800

    tweak: pub

--- fe/src/pages/PermissionDeny/index.tsx
+++ fe/src/pages/PermissionDeny/index.tsx
@@ -22,10 +22,7 @@ export default observer(() => {
         <span className="push-top-sm po-text-18 primary-color po-bold">
           {settingStore.settings.denyText}
         </span>
-        <a
-          className="push-top po-bold"
-          href={settingStore.settings.denyActionLink}
-        >
+        <a className="push-top po-bold text-blue-400" href={settingStore.settings.denyActionLink}>
           {settingStore.settings.denyActionText}
         </a>
       </div>
