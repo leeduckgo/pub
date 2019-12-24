87c2aeb9075fb722be9448b9e4ed881f1d377fcd
Junhong <raimonfuns@gmail.com>
2019-12-24 14:52:56 +0800

    tweak: build_cn sh

--- build_cn.sh
+++ build_cn.sh
@@ -9,7 +9,7 @@ yarn install
 yarn build
 cd /pressone/pub
 
-IMAGE_NAME="dh-cn.press.one/pressone/pub"
+IMAGE_NAME="dh-cn.press.one/pressone/${1}-pub"
 
 sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
 sudo docker build -t $IMAGE_NAME .
