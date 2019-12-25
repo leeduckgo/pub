e88e0993235aec7a495528a464158de8ea882f71
Junhong <raimonfuns@gmail.com>
2019-12-25 15:21:26 +0800

    tweak: lock port

--- fe/src/utils/endpoint.ts
+++ fe/src/utils/endpoint.ts
@@ -1,6 +1,6 @@
 export default {
   getApi: () => {
     const isDevelopment = process.env.NODE_ENV === 'development';
-    return isDevelopment ? 'http://localhost:8060' : window.location.origin;
+    return isDevelopment ? 'http://localhost:8000' : window.location.origin;
   },
 };
