ffd1dd59e893ccf8c0e4288b2ebcac47cfdc70c8
Junhong <raimonfuns@gmail.com>
2020-02-04 13:25:58 +0800

    tweak: exit process when connecting db unsuccessfully

--- be/models/sequelize/index.js
+++ be/models/sequelize/index.js
@@ -5,9 +5,18 @@ const {
 
 const db = config.db;
 
-module.exports = new Sequelize(db.database, db.user, db.password, {
+const sequelize = new Sequelize(db.database, db.user, db.password, {
   host: db.host,
   dialect: db.dialect,
   port: db.port,
   logging: config.sequelizeLogging
 });
+
+sequelize.sync().then(() => {
+  console.log('DB connected successfully.');
+}, (err) => {
+  console.log(err);
+  process.exit(0);
+});
+
+module.exports = sequelize;
